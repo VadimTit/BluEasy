9c1c3eb34344b32637a9e0c2312819ae9b9cce95
VadimTit <ozzymafiozzii@gmail.com>
2020-05-21 12:50:20 +0300

    add js gallery

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,6 @@ var header = document.getElementById("myHeader");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
-
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
