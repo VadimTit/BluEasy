e46f874ed98c81bd4fdbeab3dd7fb1f703f95818
VadimTit <ozzymafiozzii@gmail.com>
2020-05-25 16:30:34 +0300

    add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -47,4 +47,19 @@ $(function(){
     */
   });
 });
+
+// TABS
+$(function(){
+
+  $('.tab-title li').not('.active').click(function(){
+      var index = $(this).index();
+      var content = $('.tab-content li').eq(index);
+      $(this).addClass('active').siblings().removeClass('active');
+      $('.tab-content li').css('display', 'none').eq(index).css('display', 'block');
+  })
+  
+      $('.tab-title li:first').addClass('active');
+      $('.tab-content li:first').css('display', 'block');
+  
+  })
 // @import "partials/app";
